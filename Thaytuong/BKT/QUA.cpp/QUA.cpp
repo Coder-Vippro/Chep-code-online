26c540bcb368a418b5c532729949864a2fa8d889
vuquocanh1979@gmail.com <vuquocanh1979@gmail.com>
2022-10-01 20:53:42 +0700

    depzai

--- Thaytuong/BKT/QUA.cpp/QUA.cpp
+++ Thaytuong/BKT/QUA.cpp/QUA.cpp
@@ -2,23 +2,12 @@
 
 using namespace std;
 long long x,y;
-long long k=1;
-long long k2=3;
 long long dem=0;
 int main()
 {
     cin>>x>>y;
-    for(long long i=1;i<=y;i++)
-    {
-        if(k2*k>=x && k2*k<=y)
-        {
-            dem++;
-            k2=k*k2;
-            k++;
-        }
-        else if(k2*k>y)
-            break;
-    }
+    for(long long i=x;i<=y;i=i*2)
+        dem++;
     cout<<dem;
     return 0;
 }
