1870e6373a9c5e6fed28b41dcbe7ff4fba0d8d26
vuquocanh1979@gmail.com <vuquocanh1979@gmail.com>
2022-10-18 21:03:22 +0700

    Update XM.cpp

--- Thaytuong/CHOIBAI/CHOIBAI.cpp
+++ Thaytuong/CHOIBAI/CHOIBAI.cpp
@@ -10,14 +10,14 @@ int main()
     for(int i=1;i<=n;i++)
     {
         cin>>A[i];
-
     }
-    sort(A,A+n+1);
+    sort(A+1,A+n+1);
+    reverse(A+1,A+n+1);
     int dem1=0,dem2=0;
     for(int i=1;i<=n;i++)
     {
         if(i%2==1)dem1=dem1+A[i];
-        else if(i%2==0)dem2=dem2+A[i];
+        else dem2=dem2+A[i];
     }
     cout<<dem1<<" "<<dem2;
     return 0;
