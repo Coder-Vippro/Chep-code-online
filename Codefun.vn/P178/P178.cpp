1c4deb964fec23932d1146a6d7699b17fde65511
vuquocanh1979@gmail.com <vuquocanh1979@gmail.com>
2022-10-23 07:30:47 +0700

    depzai

--- Codefun.vn/P178/P178.cpp
+++ Codefun.vn/P178/P178.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 int A[100001];
+int f[100001];
 int main()
 {
     int n;
     cin>>n;
-    int dem=0;
-    int maxx=0;
     for(int i=1;i<=n;i++)
     {
         cin>>A[i];
     }
-    for(int i=1;i<=n;i++)
+    f[1]=1;
+    for(int i=2;i<=n;i++)
     {
-        if(A[i-1]<A[i])
-        dem++;
-        else {cout<<dem; return 0;}
-        if(maxx<dem)
-        maxx=dem;
+        if(A[i]>A[i-1])
+        {
+            f[i]=f[i-1]+1;
+        }
+        else if(A[i-1]>A[i])f[i]=1;
     }
+    sort(f,f+n+1);
+    cout<<f[n];
+    
 }
