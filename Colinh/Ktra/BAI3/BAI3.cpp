58ad8e72d98c130c4b8b4721cc01eabf8cbb10a4
vuquocanh1979@gmail.com <vuquocanh1979@gmail.com>
2022-10-26 14:45:06 +0700

    depzai

--- Colinh/Ktra/BAI3/BAI3.cpp
+++ Colinh/Ktra/BAI3/BAI3.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+string s;
 int main()
 {
-    string s;
+    freopen("BAI3.INP","r",stdin);
+    freopen("BAI3.OUT","w",stdout);
     cin>>s;
     string k=s;
     reverse(s.begin(),s.end());
