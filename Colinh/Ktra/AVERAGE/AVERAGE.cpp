98914571ad41f168d303a4c11f93cf661ab9d3e6
vuquocanh1979@gmail.com <vuquocanh1979@gmail.com>
2022-10-26 14:56:26 +0700

    update

--- Colinh/Ktra/AVERAGE/AVERAGE.cpp
+++ Colinh/Ktra/AVERAGE/AVERAGE.cpp
@@ -5,8 +5,8 @@ ll n;
 double A[100001];
 int main()
 {
-    freopen("BAI1.INP","r",stdin);
-    freopen("BAI1.OUT","w",stdout);
+    freopen("AVERAGE.INP","r",stdin);
+    freopen("AVERAGE.OUT","w",stdout);
     cin>>n;
     double k=n;
     double tong=0;
