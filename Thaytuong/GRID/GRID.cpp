527124d703f3952f8274b4fdb108fa1e484e6530
cauvang32 <113093128+cauvang32@users.noreply.github.com>
2022-11-15 13:05:06 +0000

    depzai

--- Thaytuong/GRID/GRID.cpp
+++ Thaytuong/GRID/GRID.cpp
@@ -3,6 +3,7 @@ using namespace std;
 char a[1000][1000];
 int f[1000][1000];
 int n,m;
+const int MOD=1e9+7;
 int main()
 {
     freopen("GRID.inp","r",stdin);
@@ -19,29 +20,31 @@ int main()
                 cin>>a[i][j];
             }
         }
-    f[1][1]=0;
+    f[1][1]=1;
     for(int j=2;j<=m;j++)
     {
         if(a[1][j]!=42)
-        f[1][j]=f[1][j-1]++;
+        f[1][j]=1;
         else if(a[1][j]==42)
-        f[1][j]=f[1][j-1];
+        f[1][j]=0;
     }
     for(int i=2;i<=n;i++)
     {
         if(a[i][1]!=42)
-        f[i][1]=f[i-1][1]++;
+        f[i][1]=1;
         else if(a[i][1]==42)
-        f[i][1]=f[i-1][1];
+        f[i][1]=0;
         //f[i][1]=a[i][1]+f[i-1][1];
     }
     for(int i=2;i<=n;i++)
         {
             for(int j=2;j<=m;j++)
             {
-                f[i][j]=max(f[i-1][j],f[i][j-1]);
+                if(a[i][j]!=42)
+                f[i][j]=(f[i-1][j]+f[i][j-1])%MOD;
+                else f[i][j]=0;
             }
         }
-    cout<<f[n][m]+1<<endl;
+    cout<<f[n][m];
     return 0;
 }
