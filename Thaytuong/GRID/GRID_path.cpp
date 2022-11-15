e6893e2792fa5dfa70b6c5c4df261b119fce7a41
cauvang32 <113093128+cauvang32@users.noreply.github.com>
2022-11-15 13:13:44 +0000

    de

--- Thaytuong/GRID/GRID_path.cpp
+++ Thaytuong/GRID/GRID_path.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 char a[1000][1000];
-int f[1000][1000];
-int n,m;
+long long f[1000][1000];
+long long n,m;
 const int MOD=1e9+7;
 int main()
 {
-    freopen("GRID.inp","r",stdin);
-    freopen("GRID.out","w",stdout);
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    freopen("GRID_path.inp","r",stdin);
+    freopen("GRID_path.out","w",stdout);
     cin>>n;
     int m=n;
     for(int i=1;i<=n;i++)
@@ -26,15 +23,14 @@ int main()
         if(a[1][j]!=42)
         f[1][j]=1;
         else if(a[1][j]==42)
-        f[1][j]=0;
+        {f[1][j]=0;break;}
     }
     for(int i=2;i<=n;i++)
     {
         if(a[i][1]!=42)
         f[i][1]=1;
         else if(a[i][1]==42)
-        f[i][1]=0;
-        //f[i][1]=a[i][1]+f[i-1][1];
+        {f[i][1]=0;break;}
     }
     for(int i=2;i<=n;i++)
         {
