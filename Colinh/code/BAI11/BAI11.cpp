370b16cafe1e802b0d0480cd1a4cde51332e1901
cauvang32 <113093128+cauvang32@users.noreply.github.com>
2022-11-16 10:49:01 +0000

    a

--- Colinh/code/BAI11/BAI11.cpp
+++ Colinh/code/BAI11/BAI11.cpp
@@ -1,16 +1,28 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define N int (1e7)
 using namespace std;
-int gt=2;i=2;
-double e=2;
+int p[N+2];
+int n;
+void sangngto (int n) 
+{
+    fill(p,p+n+1,1); 
+    p[0]=p[1]=0;
+    for(int i=2;i*i<=n;i++)
+    {
+        if(p[i]==1)
+        for(int j=i*i;j<=n;j+=i) p[j]=0;
+    }
+}
+
 int main()
 {
-    while(1.0/gt>=2e-6)
-    {
-        e=e+1.0/gt;
-        i++;
-        gt=gt*i;
-    }
-    cout<<fixed<<setprecision(4)<<e;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin>>n;
+    sangngto(n);
+    for(int i=2;i<=n;i++)
+    if(p[i]==1)cout<<i<<" ";
     return 0;
 }
