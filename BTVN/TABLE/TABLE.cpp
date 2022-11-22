5bd924c62bb53320c9e0dd70f680eb69fb6868b2
cauvang32 <113093128+cauvang32@users.noreply.github.com>
2022-11-22 14:58:23 +0000

    a

--- BTVN/TABLE/TABLE.cpp
+++ BTVN/TABLE/TABLE.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    freopen("TABLE.INP", "r",stdin);
-    freopen("TABLE.OUT", "w",stdout);
+    //freopen("TABLE.INP", "r",stdin);
+    //freopen("TABLE.OUT", "w",stdout);
     int a,b,c,d;
     cin>>a>>b>>c>>d;
     int maxx=max(a,b);
