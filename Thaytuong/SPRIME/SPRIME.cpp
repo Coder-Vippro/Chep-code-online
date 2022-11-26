243cbb9d22676d59bc69e1ae0f369a5d2db8b417
cauvang32 <113093128+cauvang32@users.noreply.github.com>
2022-11-26 13:12:45 +0000

    Sprime.cpp fix

--- Thaytuong/SPRIME/SPRIME.cpp
+++ Thaytuong/SPRIME/SPRIME.cpp
@@ -21,14 +21,14 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int n;
+    int n,x;
     cin>>n;
     sangngto(100000);
     while(n--)
     {
         cin>>x;
-        if(f[x]==1)cout<<"YES";
-        else cout<<"NO";
+        if(p[x]==1)cout<<"YES"<<'\n';
+        else cout<<"NO"<<'\n';
     }
     return 0;
 }
