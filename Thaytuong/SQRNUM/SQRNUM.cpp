5cd9caaa8b1c2d09f79e3cbe92afc5cfc3b12e71
cauvang32 <113093128+cauvang32@users.noreply.github.com>
2022-11-26 14:16:27 +0000

    a

--- Thaytuong/SQRNUM/SQRNUM.cpp
+++ Thaytuong/SQRNUM/SQRNUM.cpp
@@ -3,33 +3,28 @@
 using namespace std;
 int p[N+2];
 int n;
-void sangngto (int n) 
+long long kt(long long k)
 {
-    fill(p,p+n+1,1);
-    p[0]=p[1]=0;
-    for(int i=2;i*i<=n;i++)
-    {
-        if(p[i]==1)
-        for(int j=i*i;j<=n;j+=i) p[j]=0;
-    }
+    long long n=sqrt(k);
+    if(k==n*n)
+    return true;
+    return false;
 }
-
 int main()
 {
-    //freopen("SQRNUM.INP","r",stdin);
-    //freopen("SQRNUM.OUT","w",stdout);
+    freopen("SQRNUM.INP","r",stdin);
+    freopen("SQRNUM.OUT","w",stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int n,x;
+    long long n,x;
     cin>>n;
-    int k=1;
-    sangngto(100000);
+    long long k=1;
     while(n--)
     {
         cin>>x;
         k=k*x;
-        if(p[x]==1)cout<<"YES"<<'\n';
+        if(kt(k)==true)cout<<"YES"<<'\n';
         else cout<<"NO"<<'\n';
     }
     return 0;
