63dfd4fd5528bce34e7b17448b4b64948bc82020
cauvang32 <quocanh08@cauvang32.wip.la>
2022-12-03 20:31:15 +0700

    burh

--- Thaytuong/BKT/GHEPSO/GHEPSO.cpp
+++ Thaytuong/BKT/GHEPSO/GHEPSO.cpp
@@ -4,7 +4,7 @@ int A[4];
 int main()
 {
     freopen("GHEPSO.INP","r",stdin);
-    freopne("GHEPSO.OUT","w",stdout);
+    freopen("GHEPSO.OUT","w",stdout);
     cin>>A[1]>>A[2]>>A[3];
     sort(A+1,A+4);
     cout<<A[3]<<A[2]<<A[1];
