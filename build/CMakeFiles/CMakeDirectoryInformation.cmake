379cf91338caacff64e67656b1441a8aabc20e93
cauvang32 <quocanh08@cauvang32.wip.la>
2022-12-07 15:55:07 +0700

    depzai

--- build/CMakeFiles/CMakeDirectoryInformation.cmake
+++ build/CMakeFiles/CMakeDirectoryInformation.cmake
@@ -3,7 +3,7 @@
 
 # Relative path conversion top directories.
 set(CMAKE_RELATIVE_PATH_TOP_SOURCE "/home/cauvang/Desktop/Code/build")
-set(CMAKE_RELATIVE_PATH_TOP_BINARY "/home/cauvang/Desktop/Code")
+set(CMAKE_RELATIVE_PATH_TOP_BINARY "/home/cauvang/Desktop/Code/build/build")
 
 # Force unix paths in dependencies.
 set(CMAKE_FORCE_UNIX_PATHS 1)
