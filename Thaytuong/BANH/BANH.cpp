e803eb968583a2015ff5f01b4d7f55b170a5718e
cauvang32 <quocanh08@cauvang32.wip.la>
2022-12-10 21:00:12 +0700

    a

---
+++ Thaytuong/BANH/BANH.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int A[1000001];
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>A[i];
+        tong=tong+A[i];
+    }
+    if(x-tong<0)
+    {
+        
+        return 0
+    }
+}
