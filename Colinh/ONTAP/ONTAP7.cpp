da9734201cda838ee456671f63ab9a5a320792a5
cauvang32 <quocanh08@cauvang32.wip.la>
2022-12-14 14:25:41 +0700

    abcd

--- Colinh/ONTAP/ONTAP7.cpp
+++ Colinh/ONTAP/ONTAP7.cpp
@@ -1,1 +1,19 @@
-#include <bit
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    freopen("ONTAP7.INP","r",stdin);
+    freopen("ONTAP7.OUT","w",stdout);
+    string s;
+    getline(cin,s);
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]!='Z' && s[i]!=' ')
+        {
+            s[i]=char(s[i]-1);
+        }
+        else if(s[i]!=' ')s[i]='Z';
+    }
+    cout<<s;
+
+}
--- Colinh/ONTAP/debug/test.cpp
+++ Colinh/ONTAP/debug/test.cpp
@@ -1,21 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+string giaima(string s)
+{
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]!=' ')
+        {
+            if(s[i]!='A')
+            s[i]=char(s[i]-1);
+            else if(s[i]=='A')s[i]='Z';
+        }
+    }
+    return s;
+}
 string s;
+int n;
 int main()
 {
-    freopen("ONTAP6.INP","r",stdin);
-    //freopen("ONTAP6.OUT","w",stdout);
-    freopen("kq.out","w",stdout);
-    cin>>s;
-    char tmp=s[0];
-    cout<<tmp;
-    for(int i=1;i<s.size();i++)
+    freopen("ONTAP7.INP","r",stdin);
+    //freopen("ONTAP7.OUT","w",stdout);
+    freopen("kq.OUT","w",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    while(cin>>n)
     {
-        if(s[i]!=tmp)
+        getline(cin,s);
+        for(int i=1;i<=n;i++)
         {
-            cout<<s[i];
-            tmp=s[i];
+            s=giaima(s);
         }
+        cout<<s<<'\n';
     }
     
 }
