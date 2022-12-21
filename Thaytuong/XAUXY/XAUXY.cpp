01aeba71f851ac0167541c84767ba512cbaa62f0
cauvang32 <vuquocanh1979@gmail.com>
2022-12-21 22:09:31 +0700

    ad

---
+++ Thaytuong/XAUXY/XAUXY.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    freopen("XAUXY.INP","r",stdin);
+    freopen("XAUXY.OUT","w",stdout);
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    int dem=0;
+    for(int i=1;i<s.size;i++)
+    {
+        if(s[i]!=s[i-1])dem++;
+    }
+    cout<<dem;
+    return 0;
+}
