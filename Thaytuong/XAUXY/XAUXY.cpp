9d120284994a8f1789be5f89ca32e04028937114
cauvang32 <quocanh08@cauvang.wip.la>
2022-12-24 21:07:14 +0700

    a

--- Thaytuong/XAUXY/XAUXY.cpp
+++ Thaytuong/XAUXY/XAUXY.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 string s;
 int kq=0;
+int x;
 int main()
 {
     freopen("XAUXY.INP","r",stdin);
     freopen("XAUXY.OUT","w",stdout);   
-    cin>>s;
+    cin>>x>>s;
     int n=s.size();
     string x,y;
-    for(int vt=0;vt<=n-1;vt++)
+    for(int vt=0;vt<n-1;vt++)
     {
         x=s.substr(0,vt);
         y=s.substr(vt,n);
