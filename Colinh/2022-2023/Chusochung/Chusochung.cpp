0236ccbfc55d30ad1acd4d1e1b80ecbbe2d9b44f
cauvang32 <quocanh08@cauvang.wip.la>
2022-12-25 14:47:57 +0700

    done 2022-2023

--- Colinh/2022-2023/Chusochung/Chusochung.cpp
+++ Colinh/2022-2023/Chusochung/Chusochung.cpp
@@ -5,8 +5,15 @@
 using namespace std;
 int main()
 {
+    freopen("Chusochung.INP","r",stdin);
+    freopen("Chusochung.OUT","w",stdout);
     string s;
     string k;
     cin>>s>>k;
-    
+    int dem=0;
+    for(int i=0;i<s.size();i++)
+    {
+        if(k.find(s[i])!=k.npos){dem++;}
+    }
+    cout<<dem;
 }
