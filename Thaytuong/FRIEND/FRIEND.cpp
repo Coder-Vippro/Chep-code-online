44c96bf5b0a2135d53a08b23286c22a2f1b5db01
cauvang32 <quocanh08@cauvang.wip.la>
2022-12-31 20:38:10 +0700

    a

--- Thaytuong/FRIEND/FRIEND.cpp
+++ Thaytuong/FRIEND/FRIEND.cpp
@@ -3,8 +3,23 @@
 #include <algorithm>
 using namespace std;
 int A[1000001];
-int x;
 int dem=0;
+int binarysearch (int i, int j, int key)
+{
+    int l=i,r=j;
+    while(l<=r)
+    {
+        int mid=(l+r)/2;
+        if(A[mid]==key)
+        return mid;
+        else if(A[mid]>key)
+        {
+            r=mid-1;
+        }
+        else l=mid+1;
+    }
+    return -1;
+}
 int main()
 {
     freopen("FRIEND.INP","r",stdin);
@@ -12,17 +27,19 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int n;
-    cin>>n>>x
+    int n,x;
+    cin>>n>>x;
+    int key;
     for(int i=1;i<=n;i++)
     {
         cin>>A[i];
     }
+    sort(A+1,A+n+1);
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<i;j++)
+        key=x-A[i];
+        if(binarysearch(i+1,n,key)!=-1 && key>A[i])
         {
-            if(A[i]+A[j]==x)
             dem++;
         }
     }
--- Thaytuong/SUMX/SUMX.cpp
+++ Thaytuong/SUMX/SUMX.cpp
@@ -4,6 +4,22 @@
 using namespace std;
 int A[1000001];
 int dem=0;
+int binarysearch (int i, int j, int key)
+{
+    int l=i,r=j;
+    while(l<=r)
+    {
+        int mid=(l+r)/2;
+        if(A[mid]==key)
+        return mid;
+        else if(A[mid]>key)
+        {
+            r=mid-1;
+        }
+        else l=mid+1;
+    }
+    return -1;
+}
 int main()
 {
     freopen("SUMX.INP","r",stdin);
@@ -13,16 +29,18 @@ int main()
     cout.tie(0);
     int n;
     cin>>n;
+    int key;
     for(int i=1;i<=n;i++)
     {
         cin>>A[i];
     }
     int x;cin>>x;
+    sort(A+1,A+n+1);
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<i;j++)
+        key=x-A[i];
+        if(binarysearch(i+1,n,key)!=-1)
         {
-            if(A[i]+A[j]==x)
             dem++;
         }
     }
