8a95cc63cfb216730e33019560d3d69f506f2d05
cauvang32 <quocanh08@cauvang.wip.la>
2023-01-07 20:31:50 +0700

    depzai

--- Thaytuong/DICE/DICE.cpp
+++ Thaytuong/DICE/DICE.cpp
@@ -1,27 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dem(int n)
+void findWays(int N)
 {
-    if (n == 0) 
-    {
-        return 1;
-    }
-    int cnt = 0;
-    for (int i = 1; i <= 6; i++) 
-    {
-        if (n - i >= 0) 
+    int dp[N + 1];
+    dp[0] = 1;
+    for (int i = 1; i <= N; i++)
         {
-            cnt = cnt + dem(n - i);
+            dp[i] = 0;
+            for (int j = 1; j <= 6; j++)
+            {
+                if (i - j >= 0)
+                {
+                        dp[i] = dp[i] + dp[i - j];    
+                }
+            }
         }
-    }
-    return cnt;
+        cout << dp[N];
 }
 int main()
 {
-    freopen("DICE.INP","r",stdin);
-    freopen("DICE.OUT","w",stdout);
+    freopen("DICE.INP", "r", stdin);
+    freopen("DICE.OUT", "w", stdout);
     int n;
-    cin>>n;
-    cout<<dem(n);
+    cin>> n;
+    findWays(n);
     return 0;
+    //source code from https://www.geeksforgeeks.org/count-ways-to-obtain-given-sum-by-repeated-throws-of-a-dice/ 
 }
