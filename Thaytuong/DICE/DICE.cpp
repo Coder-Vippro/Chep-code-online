f18f360a425832a3ef0573a0664963584ce9d657
cauvang32 <quocanh08@cauvang.wip.la>
2023-01-07 20:08:19 +0700

    abcd

--- Thaytuong/DICE/DICE.cpp
+++ Thaytuong/DICE/DICE.cpp
@@ -1,29 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int findWays(int N)
+int dem(int n)
 {
-    if (N == 0) 
+    if (n == 0) 
     {
         return 1;
     }
     int cnt = 0;
     for (int i = 1; i <= 6; i++) 
     {
-        if (N - i >= 0) 
+        if (n - i >= 0) 
         {
-            cnt = cnt + findWays(N - i);
+            cnt = cnt + dem(n - i);
         }
     }
     return cnt;
 }
-
-
 int main()
 {
     freopen("DICE.INP","r",stdin);
     freopen("DICE.OUT","w",stdout);
     int n;
     cin>>n;
-    cout << findWays(N);
+    cout<<dem(n);
     return 0;
 }
